48cf7b675657d9e652a3009ed222e81593e80530
Markus Himmel <markus@himmel-villmar.de>
2014-12-06 00:51:46 +0000

    Almost complete version of DOCTranslator.cpp. Still missing settings, probably not even close to compiling, let alone working.

--- DOCTranslator.cpp
+++ DOCTranslator.cpp
@@ -3,6 +3,11 @@
 // This file is distributed under the terms of the MIT license.
 
 #include "DOCTranslator.h"
+#include <cstring>
+#include <stdio.h>
+#include <iostream>
+#include <FindDirectory.h>
+#include <Entry.h>
 
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "DOCTranslator"
@@ -50,12 +55,58 @@ DOCTranslator::DOCTranslator()
 {
 }
 
+int32
+msoffice_signature_cmp(const uint8 *bytes)
+{
+  uint8 msoffice_signature[8] = { 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A,
+          0xE1 };
+
+  size_t signature_length = 8;
+
+  return ((int32)(memcmp(msoffice_signature, bytes, signature_length)));
+}
+
+status_t
+identify_msoffice_header(BPositionIO *inSource, translator_info *outInfo)
+{
+  const int32 signatureSize = 8;
+
+  uint8 buffer[signatureSize];
+
+  if (inSource->Read(buffer, signatureSize) != signatureSize)
+  {
+    // Something went wrong reading the signature
+    return B_NO_TRANSLATOR;
+  }
+
+  if (msoffice_sig_cmp(buffer))
+  {
+    return B_NO_TRANSLATOR;
+  }
+
+  if (outInfo)
+  {
+    outInfo->type = B_DOC_FORMAT;
+    outInfo->group = B_TRANSLATOR_TEXT;
+    outInfo->quality = DOC_IN_QUALITY;
+    outInfo->capability = DOC_IN_QUALITY;
+    strcpy(outInfo->MIME, "application/msword");
+    strlcpy(outInfo->name, B_TRANSLATE("Microsoft Word document"),
+                    sizeof(outInfo->name));
+  }
+
+  return B_OK;
+}
+
 status_t
 DOCTranslator::DerivedIdentify(BPositionIO *source,
                 const translation_format *inFormat, BMessage *ioExtension,
                 translator_info *outInfo, uint32 outType)
 {
-  // This part needs code!
+  // This only checks if a given file has the header shared by all binary
+  // Microsoft office formats. I do not think it is easily possible to detect
+  // Word documents in particular.
+  return identify_msoffice_header(source, outInfo);
 }
 
 status_t
@@ -63,8 +114,72 @@ DOCTranslator::DerivedTranslate(BPositionIO *inSource,
                 const translator_info *inInfo, BMessage *ioExtension,
                 uint32 outType, BPositionIO *outDestination, int32 baseType)
 {
-  // Here be dragons.
+  // Note: BaseType will always be -1 because this is a B_TRANSLATOR_TEXT
+
+
+  if (identify_ms_office_header(inSource, NULL) != B_OK)
+  {
+    return B_NO_TRANSLATOR;
+  }
+
+  // Reset the cursor as it was progressed while identifying the header.
+  inSource->Seek(0, SEEK_SET);
+
+  // Antiword needs the input to be present as a file
+  BPath tmpDir;
+  if (find_directory(B_COMMON_TEMP_DIRECTORY, &tmpDir) != B_OK)
+  {
+    return B_ERROR;
+  }
+
+  tmpDir.Append("doctranslator_tmp.doc");
+
+  size_t bufferSize = inSource->GetSize();
+
+  const uint8 fileBuffer[bufferSize];
+
+  Read(fileBuffer, bufferSize);
+
+  ofstream inputFile;
+
+  inputFile.open(tmpDir.Path(), ios::out | ios::binary);
+
+  if (!inputFile)
+  {
+    return B_ERROR;
+  }
+
+  inputFile.write(fileBuffer, bufferSize);
+
+  // Now execute antiword
+
+  FILE *antiwordHandle = popen("antiword " << tmpDir.Path(), "r");
+
+  if (!antiwordHandle)
+  {
+    return B_ERROR;
+  }
+
+  fseek(antiwordHandle, 0, SEEK_END);
+  size_t outputLength = ftell(antiwordHandle);
+  fseek(f, 0, SEEK_SET);
+
+  uint8 outputBuffer[outputLength];
+
+  fread(outputBuffer, 1, outputLength, antiwordHandle);
+
+  pclose(antiwordHandle);
+
+  outDestination->Write(outputBuffer, outputLength);
 }
 
+
 BView*
-DOCTranslator::
+DOCTranslator::NewConfigView(TranslatorSettings *settings)
+{
+  return new DOCView(BRect(0, 0, DOC_VIEW_WIDTH, PNG_VIEW_HEIGHT),
+                  B_TRANSLATE("DOCTranslator Settings"), B_FOLLOW_ALL,
+                  B_WILL_DRAW, settings);
+}
+
+}
